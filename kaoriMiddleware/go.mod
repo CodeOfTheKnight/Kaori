f10a27937dcbebc42a1d677a02dee19fd92c86aa
Diego <72557769+KiritoNya@users.noreply.github.com>
2021-05-23 23:14:08 +0200

    Add files via upload

---
+++ kaoriMiddleware/go.mod
@@ -0,0 +1,12 @@
+module github.com/CodeOfTheKnight/Kaori/kaoriMiddleware
+
+go 1.16
+
+require (
+	github.com/CodeOfTheKnight/Kaori v0.4.4
+	github.com/CodeOfTheKnight/Kaori/kaoriDatabase v0.0.0-20210523151639-0f9a753d6ec5
+	github.com/CodeOfTheKnight/Kaori/kaoriJwt v0.0.0-20210523161757-4a4804303a82
+	github.com/CodeOfTheKnight/Kaori/kaoriLog v0.0.0-20210523211020-96bdaaaea141
+	github.com/CodeOfTheKnight/Kaori/kaoriUtils v0.0.0-20210523201127-0dda60540286
+	github.com/sirupsen/logrus v1.8.1
+)
---
+++ kaoriMiddleware/middlewares.go
@@ -0,0 +1,163 @@
+package kaoriMiddleware
+
+import (
+	"context"
+	"github.com/CodeOfTheKnight/Kaori/kaoriDatabase"
+	"github.com/CodeOfTheKnight/Kaori/kaoriJwt"
+	"github.com/CodeOfTheKnight/Kaori/kaoriLog"
+	"github.com/CodeOfTheKnight/Kaori/kaoriSettings"
+	"github.com/CodeOfTheKnight/Kaori/kaoriUtils"
+	logger "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type AuthMiddlewarePerm struct {
+	PermRequired []kaoriJwt.Permission
+}
+
+type ContextValues struct {
+	m map[string]string
+}
+
+func (v ContextValues) Get(key string) string {
+	return v.m[key]
+}
+
+func NewAuthMiddlewarePerm(perms ...kaoriJwt.Permission) *AuthMiddlewarePerm {
+	var amp AuthMiddlewarePerm
+	for _, p := range perms {
+		amp.PermRequired = append(amp.PermRequired, p)
+	}
+	return &amp
+}
+
+func (amp *AuthMiddlewarePerm) Authmiddleware(kaoriDb *kaoriDatabase.NoSqlDb, accessKey string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		ip := kaoriUtils.GetIP(r)
+
+		//Extract token JWT
+		tokenString := kaoriJwt.ExtractToken(r)
+		metadata, err := kaoriJwt.ExtractAccessMetadata(tokenString, accessKey)
+		if err != nil {
+			kaoriLog.PrintLog("General", ip, "authMiddleware", "Error to get access token metadata: "+err.Error(), 1)
+			kaoriUtils.PrintErr(w, "Token not valid")
+			return
+		}
+
+		permissions, err := metadata.GetPermission()
+		if err != nil {
+			kaoriLog.PrintLog(metadata.Email, ip, "authMiddleware", "Error to get permissions: "+err.Error(), 1)
+			kaoriUtils.PrintInternalErr(w)
+			return
+		}
+
+		if !kaoriJwt.IsAuthorized(permissions, amp.PermRequired...) {
+			kaoriLog.PrintLog(metadata.Email, ip, "authMiddleware", "Warning permission denied!", 2)
+			http.Error(w, `{"code": 403, "msg": "You need permissions to access the service!"}`, http.StatusForbidden)
+			return
+		}
+
+		v := ContextValues{map[string]string{
+			"email": metadata.Email,
+			"ip": ip,
+		}}
+		ctx := context.WithValue(r.Context(), "values", v)
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
+}
+
+func RefreshMiddleware(kaoriUserDB *kaoriDatabase.NoSqlDb, cfg *kaoriSettings.Config, loginUrl string, next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		ip := kaoriUtils.GetIP(r)
+
+		//Get token from cookies
+		cookieData, err := kaoriUtils.GetCookies(r, cfg.Jwt.Iss, cfg.Password.Cookies)
+		if err != nil {
+			kaoriLog.PrintLog("General", ip, "ApiRefresh", "Error to get cookies: "+err.Error(), 1)
+			redirect, err := kaoriUtils.ParseTemplate(cfg.Template.Html["redirect"], loginUrl)
+			if err != nil {
+				logger.WithFields(logger.Fields{
+					"function": "refreshMiddleware",
+					"ip": ip,
+				}).Error("Unable to parse email template")
+				kaoriUtils.PrintInternalErr(w)
+				return
+			}
+			w.Write([]byte(redirect))
+			return
+		}
+
+		token := cookieData["RefreshToken"]
+
+		//Extract data from token
+		data, err := kaoriJwt.ExtractRefreshMetadata(token, cfg.Password.RefreshToken)
+		if err != nil {
+			kaoriLog.PrintLog("General", ip, "ApiRefresh", "Extract refresh token error: "+err.Error(), 1)
+			redirect, err := kaoriUtils.ParseTemplate(cfg.Template.Html["redirect"], loginUrl)
+			if err != nil {
+				logger.WithFields(logger.Fields{
+					"function": "refreshMiddleware",
+					"ip": ip,
+				}).Error("Unable to parse email template")
+				kaoriUtils.PrintInternalErr(w)
+				return
+			}
+			w.Write([]byte(redirect))
+			return
+		}
+
+		//Check validity
+		if !kaoriJwt.VerifyRefreshToken(kaoriUserDB, data.Email, data.RefreshId) {
+			kaoriLog.PrintLog(data.Email, ip, "ApiRefresh", "Token not valid", 2)
+			redirect, err := kaoriUtils.ParseTemplate(cfg.Template.Html["redirect"], loginUrl)
+			if err != nil {
+				logger.WithFields(logger.Fields{
+					"function": "refreshMiddleware",
+					"ip": ip,
+				}).Error("Unable to parse email template")
+				kaoriUtils.PrintInternalErr(w)
+				return
+			}
+			w.Write([]byte(redirect))
+			return
+		}
+
+		if !kaoriJwt.VerifyExpireDate(data.Exp) {
+			kaoriLog.PrintLog(data.Email, ip, "ApiRefresh", "Token expired", 2)
+			redirect, err := kaoriUtils.ParseTemplate(cfg.Template.Html["redirect"], loginUrl)
+			if err != nil {
+				logger.WithFields(logger.Fields{
+					"function": "refreshMiddleware",
+					"ip": ip,
+				}).Error("Unable to parse email template")
+				kaoriUtils.PrintInternalErr(w)
+				return
+			}
+			w.Write([]byte(redirect))
+			return
+		}
+
+		w.Write([]byte(`{"code": 200, "msg": "OK"}`))
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+
+	})
+}
+
+func EnableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		next.ServeHTTP(w, r)
+	})
+}
